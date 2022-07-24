f27c55c598c290a49c21fff20e7173782f82e804
dxw <dxw@163.com>
2022-07-24 15:40:49 +0800

    foufirstsecond commit

--- git-demo/src/main/java/com/hspedu/GitTest.java
+++ git-demo/src/main/java/com/hspedu/GitTest.java
@@ -3,6 +3,8 @@ package com.hspedu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
+        System.out.println("hello,git3");
 
 
     }
